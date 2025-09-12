1589681124b587eaae3bbecac1d2cc84c6aeb2b3
JokinU-96 <jokin.urtaga@ikasle.egibide.org>
2025-09-12 20:24:25 +0200

    Adaptado a MAC

--- NuevoProyecto/src/Ejercicio4.java
+++ NuevoProyecto/src/Ejercicio4.java
@@ -1,15 +1,20 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Ejercicio4 {
     public static void main(String[] args) {
-        ProcessBuilder pb = new ProcessBuilder("konsole", "-e", "ls -l");
+        ProcessBuilder pb = new ProcessBuilder("bash","-c", "ls -l");
         Process proceso = null;
 
         Runtime rt = Runtime.getRuntime();
 
         try {
+
+            pb.redirectOutput(new File("salida.txt"));
+            pb.redirectError(new File("error.txt"));
+
             proceso = pb.start();
 
             BufferedReader br = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
